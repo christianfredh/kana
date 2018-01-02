4df65b1de08a4cae3c9ed1d2a2e2d0a2ee3e0586
Christian Fredh <christian.fredh@gmail.com>
2018-01-03 00:17:07 +0100

    Support aswer horagana and katakana at the same time

--- character-loader.js
+++ character-loader.js
@@ -6,14 +6,21 @@ const containers = document.querySelectorAll('.js_characters');
 
     const questionKey = container.getAttribute('data-question-key');
     const answerKey = container.getAttribute('data-answer-key');
+    const answerCount = parseInt(container.getAttribute('data-answer-count') || '1');
+
+    const answerKeys = answerKey && answerKey.split(',') || [];
 
     shuffledCharacters.forEach(function(character) {
 
-        const box = `
+        let box = `
         <div class="character-box">
-            <div class="question">${questionKey && character[questionKey]}</div>
-            <div class="answer">${answerKey && character[answerKey] || ''}</div>
-        </div>`;
+            <div class="question">${questionKey && character[questionKey]}</div>`;
+
+        for (answerIndex = 0; answerIndex < answerCount; answerIndex++) {
+            box += `<div class="answer">${answerKeys[answerIndex] && character[answerKeys[answerIndex]] || ''}</div>`;
+        }
+
+        box += `</div>`;
 
         container.innerHTML += box;
 
